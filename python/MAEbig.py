Added new functions for calculation of isolation, gap and optimal masses and a script that makes a plot with these as a function of radius from the host star. Also included some small fixes for m6_exe and MAEbig.

python/MAEbig.py
@@ -58,7 +58,7 @@ def readdata(filename):
 try:
     sys.argv[1]
 except (IndexError,NameError):
-    source  = 'migtest'
+    source  = 'pa+mig6'
 else:
     source = sys.argv[1] #The vars.ini file
 #    vars_   = process_input(inputfile)
@@ -171,6 +171,8 @@ for k in range(0,4):
                 axlist[k].set_xlabel(r'$\mathrm{ Time \ (yr)}$')
                 axlist[k].set_ylabel('$ {\\rm Mass \\ (M_{\\oplus})}$')
                 axlist[k].set_ylim(mmin,mmax)
+                #We check if the planet has undergone collisions and plot them
+                #if that's the case
                 if i in collinfo[0]:  
                     idx = np.where(i==collinfo[0])[0]
                     axes.plot(collinfo[2,idx],collinfo[1,idx],'X',markersize=5,markerfacecolor=ccycle[i-1],\
@@ -178,15 +180,25 @@ for k in range(0,4):
                     axlist[k].plot(collinfo[2,idx],collinfo[1,idx],'X',markersize=5,markerfacecolor=ccycle[i-1],\
                           markeredgecolor='k',markeredgewidth=0.5,zorder=2)
             if output =='semi_time':
-                axes.plot(time_big,semi_big,linewidth=lw3,color=ccycle[i-1])
+                axes.plot(time_big,semi_big,linewidth=lw3,color=ccycle[i-1],\
+                          zorder=1)
                 axes.set_xlabel('${\\rm Time \\ (yr)}$')
                 axes.set_ylabel('$ {\\rm Semimajor \\ axis \\ (AU)}$')
                 axes.set_ylim(amin,amax)
                 
-                axlist[k].plot(time_big,semi_big,linewidth=lw3,color=ccycle[i-1])
+                axlist[k].plot(time_big,semi_big,linewidth=lw3,color=ccycle[i-1],\
+                      zorder=1)
                 axlist[k].set_xlabel('${\\rm Time \\ (yr)}$')
                 axlist[k].set_ylabel('$ {\\rm Semimajor \\ axis \\ (AU)}$')
                 axlist[k].set_ylim(amin,amax)
+                #We check if the planet has reached isolation mass
+                im_idx = check_isomass(semi_big,mass_big,mdot_gas,L_s,M_s,alpha_d,alpha_v,kap,opt_vis)
+                #If it has we plot the corresponding time and semi-major axis
+                if im_idx is not None:
+                    axes.plot(time_big[im_idx],semi_big[im_idx],'p',markersize=5,markerfacecolor=ccycle[i-1],\
+                          markeredgecolor='k',markeredgewidth=0.5,zorder=2)
+                    axlist[k].plot(time_big[im_idx],semi_big[im_idx],'p',markersize=5,markerfacecolor=ccycle[i-1],\
+                          markeredgecolor='k',markeredgewidth=0.5,zorder=2)
             if output =='ecc_time':
                 axes.plot(time_big,ecc_big,linewidth=lw3,color=ccycle[i-1])
                 axes.set_xlabel('${\\rm Time \\ (yr)}$')

python/m6_exe.py
@@ -73,7 +73,7 @@ else:
 if amin in ['ice','r_ice','None','none']:
     _,alpha_v,mdot_gas,L_s,_,_,_,kap,M_s,opt_vis = get_disk_params()
     r_ice = rsnow(mdot_gas,L_s,M_s,alpha_v,kap,opt_vis)
-    avals = np.asarray([r_ice]*N)
+    avals = np.asarray([r_ice])
 elif type(amin) in [float,int]:
     amax  = amin + astep*N
     avals = np.arange(amin,amax,astep)
@@ -83,28 +83,39 @@ else:
 #We also generate the masses in the range provided
 pmass = np.linspace(mrange[0],mrange[1],N)
 
+#We set up the container for the initial data of the embryos
+
+if insert_embryo:
+    bigdata    = np.zeros((1,10))
+    bignames = ['P1']
+    pmass = np.array([mrange[0]])
+    #We draw eccentricities from a Rayleigh distr.
+    ep = np.random.rayleigh(1e-2,1) #We draw eccentricities from a Rayleigh distr.
+else:
+    bigdata    = np.zeros((N,10))
+    bignames = ['P{}'.format(i+1) for i in range(N)]
+    #We draw eccentricities from a Rayleigh distr.
+    ep = np.random.rayleigh(1e-2,N)
+    
 #Next we set up the physical properties and the phase of the planets and
 #store them in an array of size (N,10)
-mp = pmass*(Mearth/Msun)
-ep = np.random.rayleigh(1e-2,N) #We draw eccentricities from a Rayleigh distr.
 ip = 0.5*ep
 rp = 1.0
 dp = 1.5
 xp = 1.5e-9
+mp = pmass*(Mearth/Msun)
 
-props = np.array([0,rp,dp,xp,0,0,0,0,0,0])
-
-bigdata    = np.zeros((N,10))
-bigdata[:] = props
-
+props = np.array([0,rp,dp,xp,0,0,0,0,0,0])    
+    
+bigdata[:] = props    
+    
 #We also insert the semi-major axis values and masses
 bigdata[:,0] = mp
 bigdata[:,4] = avals
 bigdata[:,5] = ep
 bigdata[:,6] = ip
 
-#Finally we generate the names and write it all into our big.in file
-bignames = ['P{}'.format(i+1) for i in range(N)]
+#Finally we write it all into our big.in file
 
 big_input(bignames,bigdata,asteroidal=True)
 
@@ -128,7 +139,7 @@ t = 0
 i = 2
 while t < T:
     
-    if insert_embryo & (t>0) & (i<=20):
+    if insert_embryo & (t>0) & (i<=N):
         insert_planet_embryo(mp[0],avals[0],i)
         i += 1
     